53ffda76c10db0a160d5b011b770fb7dfdc7e5c6
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2024-02-09 08:56:29 +0900

    implement "com.microsoft.entra.verifiedid:walletlibrary:1.0.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -52,13 +54,16 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //verified
+    implementation("com.microsoft.entra.verifiedid:walletlibrary:1.0.0")
 
     //QR
     implementation("com.google.zxing:core:3.4.0")
--- app/src/main/java/org/idp/wallet/app/MainActivity.kt
+++ app/src/main/java/org/idp/wallet/app/MainActivity.kt
@@ -2,6 +2,7 @@ package org.idp.wallet.app
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -15,8 +16,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import com.microsoft.walletlibrary.VerifiedIdClientBuilder
+import com.microsoft.walletlibrary.requests.VerifiedIdPresentationRequest
+import com.microsoft.walletlibrary.requests.VerifiedIdRequest
+import com.microsoft.walletlibrary.requests.input.VerifiedIdRequestURL
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 import org.idp.wallet.app.ui.theme.VCWalletAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,17 +52,33 @@ class MainActivity : ComponentActivity() {
         val result: IntentResult =
             IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
-        if (null != result) {
-            val barcodeValue = result.contents
+        val barcodeValue = result.contents
 
-            if (null == barcodeValue) {
-                Toast.makeText(this, "Read Error", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, barcodeValue, Toast.LENGTH_LONG).show()
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+        if (null == barcodeValue) {
+            Toast.makeText(this, "Read Error", Toast.LENGTH_LONG).show()
+            return
         }
+        val errorHandler = CoroutineExceptionHandler { _, error ->
+            Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
+        }
+        this.lifecycleScope.launch(errorHandler) {
+            val verifiedIdClient = VerifiedIdClientBuilder(this@MainActivity).build()
+
+            val verifiedIdRequestUrl = VerifiedIdRequestURL(Uri.parse(barcodeValue))
+            val verifiedIdRequestResult: Result<VerifiedIdRequest<*>> =
+                verifiedIdClient.createRequest(verifiedIdRequestUrl)
+
+            if (verifiedIdRequestResult.isSuccess) {
+                val verifiedIdRequest = verifiedIdRequestResult.getOrNull()
+                val presentationRequest = verifiedIdRequest?.let {
+                    verifiedIdRequest as VerifiedIdPresentationRequest
+                }
+            } else {
+                // If an exception occurs, its value can be accessed here.
+                val exception = verifiedIdRequestResult.exceptionOrNull()
+            }
+        }
+
     }
 }
 
