b2ed4c9cfefe82f2e96cb8ca1d4619adf6a84252
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2024-06-09 15:21:45 +0900

    add function named open with param of launcher to  enable callback

--- verifiable-credentials-library/src/main/java/org/idp/wallet/verifiable_credentials_library/util/activity/Browser.kt
+++ verifiable-credentials-library/src/main/java/org/idp/wallet/verifiable_credentials_library/util/activity/Browser.kt
@@ -1,11 +1,19 @@
 package org.idp.wallet.verifiable_credentials_library.util.activity
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import androidx.activity.result.ActivityResultLauncher
 import androidx.browser.customtabs.CustomTabsIntent
 
 fun open(context: Context, uri: String) {
   val customTabsIntent = CustomTabsIntent.Builder().build()
   customTabsIntent.intent.setData(Uri.parse(uri))
   customTabsIntent.launchUrl(context, Uri.parse(uri))
+}
+
+fun open(launcher: ActivityResultLauncher<Intent>, uri: String) {
+  val customTabsIntent = CustomTabsIntent.Builder().build()
+  customTabsIntent.intent.setData(Uri.parse(uri))
+  launcher.launch(customTabsIntent.intent)
 }
